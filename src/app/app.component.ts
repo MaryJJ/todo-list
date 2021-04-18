015f51ea955a6d79c7cfeec2d14a03d3b3a40d0d
Mary <maryja.bruixa@gmail.com>
2021-04-18 13:54:59 -0400

    New component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: ` <h1>Welcome to {{ title }}!</h1> `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo-list';
